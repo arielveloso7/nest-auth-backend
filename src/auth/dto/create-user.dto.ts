909ea5017d29989ebc4c3ee94947d87877a36376
arielveloso7 <el_veloso@hotmail.com>
2024-01-16 15:24:39 +0100

    first commit

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, MinLength, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+  @IsString()
+  name: string;
+  @MinLength(6)
+  password: string;
+}
